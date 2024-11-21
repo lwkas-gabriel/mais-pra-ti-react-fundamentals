27a873322b550d09ec5858cbfc9e0cdcf91bd0de
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-11-21 18:29:49 -0300

    feat: aplicação de contador

---
+++ src/pages/Counter/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function Counter(){
+    const [number, setNumber] = useState(0);
+
+    function handleDecrement(){
+        if(number == 0){
+            setNumber((prevState) => prevState)
+        }else{
+            setNumber((prevState) => prevState - 1)
+        }
+    }
+
+    function handleIncrement(){
+        setNumber((prevState) => prevState + 1)
+    }
+
+    return (
+        <main className="d-flex flex-column align-items-center gap-4 m-auto justify-content-center my-4 w-75">
+            <h1>Bem vindo a aplicação de incremento!</h1>
+            <div className="d-flex align-items-center gap-4">
+                <button className="btn btn-primary" onClick={handleDecrement}>-</button>
+                <span>{number}</span>
+                <button className="btn btn-primary" onClick={handleIncrement}>+</button>   
+            </div>
+        </main>
+    );
+}
