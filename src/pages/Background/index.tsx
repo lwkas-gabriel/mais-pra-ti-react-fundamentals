cb6ea76a86928736de5700f6b706eeb5018c4717
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-11-21 20:12:43 -0300

    refactor: refatorando aplicação de mudança de background

--- src/pages/Background/index.tsx
+++ src/pages/Background/index.tsx
@@ -4,8 +4,10 @@ export function Background(){
     const { theme, toggleTheme } = useTheme();
     
     return (
-        <main className="min-vh-100 d-flex flex-column align-items-center gap-4 m-auto justify-content-center my-4 w-75">
-            <button className="btn btn-secondary" onClick={toggleTheme}>Alterar para tema {theme === "light" ? "escuro" : "claro"}</button>
+        <main className="container my-4 min-vh-100">
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-secondary" onClick={toggleTheme}>Alterar para tema {theme === "light" ? "escuro" : "claro"}</button>
+            </div>
         </main>
     )
 }
